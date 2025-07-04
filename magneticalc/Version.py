2abce03b2efdd86419e4c065fdf1e783b6284299
Paul Wilhelm <shredengineer@gmail.com>
2025-07-04 07:12:19 +0200

    Bump version to v1.16.0

--- magneticalc/Version.py
+++ magneticalc/Version.py
@@ -17,7 +17,7 @@
 #  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 
-__VERSION__ = "v1.15.6"  # MAJ.MIN.REV
+__VERSION__ = "v1.16.0"  # MAJ.MIN.REV
 
 
 __URL__ = "https://github.com/shredEngineer/MagnetiCalc"
